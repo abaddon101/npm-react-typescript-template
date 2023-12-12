55e2bbf3e7425feb0e0f555b7fa1fa4b6fd3cef2
abaddon101 <101891842+abaddon101@users.noreply.github.com>
2023-12-12 10:47:56 +0100

    add comment

--- src/Components/EntriesPerPageDropdown.tsx
+++ src/Components/EntriesPerPageDropdown.tsx
@@ -1,32 +1,50 @@
+// Import React and useState hook for managing component state
 import React, { useState } from 'react';
+
+// Import the Select component from the 'react-select' library
 import Select from 'react-select';
 
+// Define the prop types for the component
 interface EntriesPerPageDropdownProps {
   onChange: (value: number) => void;
 }
 
+// Define the functional component using TypeScript
 const EntriesPerPageDropdown: React.FC<EntriesPerPageDropdownProps> = ({ onChange }) => {
+  // Define the available options for entries per page
   const entriesPerPageOptions = [10, 25, 50, 100];
+
+  // Initialize the component state for the selected option
   const [selectedOption, setSelectedOption] = useState({
     value: entriesPerPageOptions[0],
     label: entriesPerPageOptions[0],
   });
 
+  // Event handler for handling the selection in the dropdown
   const handleSelect = (selectedOption: { value: number; label: number }) => {
+    // Update the component state with the selected option
     setSelectedOption(selectedOption);
+
+    // Trigger the onChange callback with the selected value
     onChange(selectedOption.value);
   };
 
+  // Render the component
   return (
     <div className="EntriesPerPageDropdown">
+      {/* Display a text label */}
       <span>Show </span>
+
+      {/* Render the Select component from 'react-select' */}
       <Select
         value={selectedOption}
         onChange={handleSelect}
+        // Map entriesPerPageOptions to the required format for react-select
         options={entriesPerPageOptions.map((option) => ({
           value: option,
           label: option,
         }))}
+        // Customize the styles for the dropdown and menu
         styles={{
           control: (provided, state) => ({
             ...provided,
@@ -40,9 +58,12 @@ const EntriesPerPageDropdown: React.FC<EntriesPerPageDropdownProps> = ({ onChang
           }),
         }}
       />
+
+      {/* Display a text label */}
       <span> entries</span>
     </div>
   );
 };
 
+// Export the component as the default export
 export default EntriesPerPageDropdown;
--- src/Components/SortIcon.tsx
+++ src/Components/SortIcon.tsx
@@ -1,22 +1,31 @@
+// Import the React library
 import React from 'react';
 
+// Define the prop types for the component
 interface SortIconProps {
   direction: 'asc' | 'desc' | undefined;
 }
 
+// Define the functional component using TypeScript
 const SortIcon: React.FC<SortIconProps> = ({ direction }) => {
+  // Define the styles for the sort icon
   const iconStyle: React.CSSProperties = {
-    fontSize: '12px', // Ajustez la taille selon vos besoins
-    color: 'white', // Couleur blanche
-    marginLeft: '5px', // Espacement à gauche pour séparer de votre texte
+    fontSize: '12px', // Adjust the size as needed
+    color: 'white', // Set the color to white
+    marginLeft: '5px', // Left spacing to separate from surrounding text
   };
 
+  // Render the component
   return (
     <div className={`sort-icon ${direction ? direction : ''}`} style={iconStyle}>
+      {/* Display the 'up' arrow if direction is 'asc' */}
       {direction === 'asc' && '↑'}
+
+      {/* Display the 'down' arrow if direction is 'desc' */}
       {direction === 'desc' && '↓'}
     </div>
   );
 };
 
+// Export the component as the default export
 export default SortIcon;
